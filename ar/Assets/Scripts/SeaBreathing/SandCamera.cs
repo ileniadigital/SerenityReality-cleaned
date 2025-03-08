cdd44d0db7734c1cb2109751128c62bebce7b3c6
ileniadigital <ileniamaietta.business@gmail.com>
2025-03-08 11:48:08 +0100

    Sand displays on canvas
    Bugs:
    - the water is not visible anymore

---
+++ ar/Assets/Scripts/SeaBreathing/SandCamera.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SandCamera : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public float distanceBehindWater = 5f;
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = cameraTransform.position;
+        newPosition.z -= Mathf.Abs(distanceBehindWater); // Keep it behind the water
+        transform.position = newPosition;
+    }
+}
