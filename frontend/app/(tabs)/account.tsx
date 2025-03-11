d69169ca9788b50ef2588ac9847b79579c532370
ileniadigital <ileniamaietta.business@gmail.com>
2025-03-11 11:05:07 +0000

    Added password input field and converted input fields from text to textinput

--- frontend/app/(tabs)/account.tsx
+++ frontend/app/(tabs)/account.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 
 const AccountPage: React.FC = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Account</Text>
+
+            {/* Name field */}
             <View style={styles.infoContainer}>
-                <Text style={styles.label}>Username:</Text>
-                <Text style={styles.value}>JohnDoe</Text>
+                <Text style={styles.label}>Name</Text>
+                <TextInput style={styles.value}>JohnDoe</TextInput>
             </View>
+
+            {/* Email field */}
             <View style={styles.infoContainer}>
-                <Text style={styles.label}>Email:</Text>
-                <Text style={styles.value}>johndoe@example.com</Text>
+                <Text style={styles.label}>Email</Text>
+                <TextInput style={styles.value}>johndoe@example.com</TextInput>
+            </View>
+
+            {/* Password field */}
+            <View style={styles.infoContainer}>
+                <Text style={styles.label}>Password</Text>
+                <TextInput style={styles.value} secureTextEntry={true}>123</TextInput>
             </View>
             <Button title="Edit Profile" onPress={() => { }} />
             <Button title="Logout" onPress={() => { }} />
@@ -28,19 +38,25 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
     },
     title: {
-        fontSize: 24,
+        fontSize: 30,
         fontWeight: 'bold',
         marginBottom: 20,
     },
     infoContainer: {
-        flexDirection: 'row',
+        flexDirection: 'column',
+        gap: 7,
         marginBottom: 10,
     },
     label: {
         fontWeight: 'bold',
         marginRight: 10,
+        fontSize: 20,
     },
     value: {
         color: '#555',
+        fontSize: 20,
+        backgroundColor: '#D9D9D9',
+        margin: 3,
+        padding: 7,
     },
 });
